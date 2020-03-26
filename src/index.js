f6939a6ac1ad8faa19918edf35fec1dbe2d6c481
jiangtao <hustme@126.com>
2020-03-26 14:19:00 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TableImage from "./TableImage";
+import  "./index.less"
+
+const ReactDemo = () => (
+    <h1>这是我的第一个 react npm 组件</h1>
+);
+
+export {
+    ReactDemo,
+    TableImage
+}
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, "./example/src/index.html"),
+    filename: "./index.html"
+});
+
+module.exports = {
+    entry: path.join(__dirname, "./example/src/app.js"),
+    output: {
+        path: path.join(__dirname, "example/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            use: "babel-loader",
+            exclude: /node_modules/
+        },{
+            test: /\.less$/,
+            use: [
+                {
+                    loader: 'style-loader', // creates style nodes from JS strings
+                },
+                {
+                    loader: 'css-loader', // translates CSS into CommonJS
+                },
+                {
+                    loader: 'less-loader',
+                    options: {
+                        javascriptEnabled: true,
+                    },
+                },
+            ],
+        },]
+    },
+    plugins: [htmlWebpackPlugin],
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    devServer: {
+        port: 8000
+    }
+};
